d17ef45ce3880c0b4137d51e965ee53b18d94d51
Tsvetomir Dimitrov <tsv.dimitrov@gmail.com>
2018-10-04 22:31:40 +0300

    Rename bash.go to cmdhandler.go

--- shell/cmdhandler.go
+++ shell/cmdhandler.go
@@ -6,6 +6,7 @@ const StderrToDevNull = " 2> /dev/null "
 // RunInBackground is a bash snippet which runs the previous command in background
 const RunInBackground = " & "
 
+// CmdHandler processes the output of a specific shell command
 type CmdHandler interface {
 	Write(p []byte) (n int, err error)
 }
