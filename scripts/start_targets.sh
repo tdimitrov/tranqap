1a4112db7ac92c7cf88e83d58995351375a16f80
Tsvetomir Dimitrov <tsv.dimitrov@gmail.com>
2018-11-09 21:31:25 +0200

    All examples are moved to separate dir

--- scripts/start_targets.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker run -d -p 127.0.0.1:2222:22 --name target1 --rm tdimitrov/rpcap-target
-docker run -d -p 127.0.0.1:2223:22 --name target2 --rm tdimitrov/rpcap-target
--- scripts/stop_targets.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker stop target1
-docker stop target2
